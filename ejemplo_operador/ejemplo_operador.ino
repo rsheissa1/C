814cac6fa0968204f3c6ffd1d7d788dfe1b6ca8b
Dr. Roberto Castañeda Sheissa <rocastaneda@uv.mx>
2022-04-18 15:15:44 -0500

    Update ejemplo_operador.ino
    Código para leer datos en ESP32.

--- ejemplo_operador/ejemplo_operador.ino
+++ ejemplo_operador/ejemplo_operador.ino
@@ -3,28 +3,49 @@
  * Por: Roberto Castañeda Sheissa
  * Fecha: 8 de abril 2022
  * 
- * Este programa permite experimentar el uso y aplicaciones
- * del operador módulo
+ * Este programa sirve para probar los operadores
+ * lógicos y capturar datos del sensor DHT11.
+ * 
+ * Connect pin 1 (on the left) of the sensor to +5V
+ * NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+ * to 3.3V instead of 5V!
+ * Connect pin 2 of the sensor to whatever your DHTPIN is
+ * Connect pin 3 (on the right) of the sensor to GROUND (if your sensor has 3 pins)
+ * Connect pin 4 (on the right) of the sensor to GROUND and leave the pin 3 EMPTY (if your sensor has 4 pins)
+ * Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+ * 
  */
 
  // Bibliotecas
-// #include <Serial.h> -> Esto es opcional
- // Constantes
+#include <DHT.h>
+
+// Definición de objetos
+#define DHTPIN 14     // Digital pin connected to the DHT sensor from ESP32
+#define DHTTYPE DHT11 // DHT 11
+
+// Constantes
+const int BOTON1 = 4;
+const int LED1 = 2;
 
 // Variables
-int dato = 0;
-double timeStart, timeFinish;
+int dato1;
+DHT dht(DHTPIN, DHTTYPE);
 
- // Definición de objetos
 
  // Condiciones iniciales - Se ejectua sólo una vez al energizar
  void setup()
  {
   // Inicio void de void setup()
   // Aquí va código
-  Serial.begin(115200); // Establece comunicación serial y velocidad de transmisión
-  Serial.println("Comunicacion Serial iniciada");
-  delay(5000);
+  pinMode (BOTON1, INPUT_PULLUP); // Configurar el pin donde se conecta el boton como entrada
+  pinMode (LED1, OUTPUT); // Configura el pin donde se conecta el led como salida
+  digitalWrite (LED1, LOW); // 0, false, LOW, 1, ture, HIGH
+
+  
+  Serial.begin(9600); // Establece comunicación serial y velocidad de transmisión
+  Serial.println("DHT11 configurado");
+  
+  dht.begin(); // Activa la función del sensore
  } // Fin void setup
 
 // Cuerpo del programa - Se ejecuta constantemente
@@ -32,34 +53,36 @@ void loop()
 { // Inicio de void loop
   // Coloca código aquí, para ejecutarse indefinidamente
 
-  timeStart = micros();
+  dato1 = digitalRead(BOTON1); // Lectura digital de boton1
+  digitalWrite(LED1,dato1);
+  
+  // Wait a few seconds between measurements.
+  delay(2000);
 
-  for (int i = 0; i < 1000; i++)
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t))
   {
-    dato++;
-    if (dato > 8)
-    {
-      dato = 0;
-    }
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
   }
 
-  timeFinish = micros();
-  Serial.print("Con if");
-  Serial.println(timeFinish - timeStart); // Imprimir valor del contador
-  delay(1000);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
 
-  timeStart = micros();
-
-  for (int i = 0; i < 1000 ; i++)
-  {
-    dato++;
-    dato %= 8;
-  }
-
-  timeFinish = micros();
-  Serial.print("Con operadores");
-  Serial.println(timeFinish - timeStart); // Imprime valor del contador
-  delay(1000);
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(F("Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C"));
   
 } // Fin void loop
 
