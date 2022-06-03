275b800ed8f8951885fc312069837c1a8be3c340
Dr. Roberto Castañeda Sheissa <96084226+rsheissa1@users.noreply.github.com>
2022-06-03 13:42:12 -0500

    Programacion ESP32
    Código para configurar DHT11 y ESP32.
    
    Se movió a otra carpeta para poder ser leído por el ArduinoIDE

--- medidor_temp_humedad/medidor/medidor.ino
+++ medidor_temp_humedad/medidor/medidor.ino
@@ -2,7 +2,7 @@
 // Opcional, se pueden agregar LEDs para indicar el estado de operación.
 // Se agregan las bibliotecas requeridas
 #include <WiFi.h> // Biblioteca para control de WiFi
-#include <PubSubClient.> // Biblioteca de conexión MQTT
+#include <PubSubClient.h> // Biblioteca de conexión MQTT
 #include "DHT.h" // Biblioteca para conexión con DHT11
 
 // Definición del pin correspondiente
@@ -31,6 +31,9 @@ PubSubClient client(espClient);
 int flashLedPin = 4; // Indica estatus de conexión
 int statusLedPin = 33; // Controlado por MQTT
 int wait = 5000; // Espera de 5 segundos para envío de mensajes MQTT
+long timeNow, timeLast; // Variables control de tiempo
+int data = 0; // Contador
+float t = 0; // Variable para temperatura
 
 // Configuración tipo de dato para el sensor
 DHT dht(DHTPIN, DHTTYPE);
@@ -92,6 +95,8 @@ void setup()
 	client.setCallback(callback);
 	// Espera para prevenir pérdida de información
 	delay(1500); // 1500 milisegundos
+
+	timeLast = millis(); // Inicia control de tiempo
 } // Fin de setup()
 
 // Función Lazo Principal
@@ -104,7 +109,75 @@ void loop()
 		// para volver a conectarse.
 		reconnect();
 	}
-	
+
 	// Ejecuta 
-	client.loop();
+	client.loop(); // Cliente esperando mensajes
+
+	timeNow = millis(); // Control de tiempo
+	if (timeNow - timeLast > wait) // Manda mensaje MQTT cada 5 segundos
+	{
+		timeLast = timeNow; // Actualiza seguimiento de tiempo
+		// data++; // Incremento a variable para enviarse por MQTT
+    t = dht.readTemperature();
+
+    // Verifica que el dato recibido sea correcto
+    // de lo contrario, no hay conexión
+    if (isnan(t))
+    {
+      Serial.println(F("No hay conexión"));
+      return;
+    }
+    
+		char dataString[8]; // Longitud de mensaje a 8 caracteres
+		//dtostrf(data,1,2,dataString); // Función AVR convierte arreglo de caracteres en String
+    dtostrf(t,1,2,dataString);
+		Serial.print("Contador "); // Solo para visualizar que el evento sucede
+		Serial.println(dataString);
+		client.publish("codigoIoT/medidor/MQTT", dataString); // Función envía datos por MQTT. Especifica tema y valor.
+	}
+}
+
+// Función para realizar una acción en caso de recibir un mensaje
+void callback(char* topic, byte* message, unsigned int length)
+{
+	// Muestra en terminal serial que un mensaje se ha recibido
+	Serial.print("Mensaje recibido del tema: ");
+	Serial.println(topic);
+
+	// Unir mensajes recibidos y guardarlos en una variable String
+	String messageTemp; // Declaración de variable donde se guardará el mensaje completo
+	for (int i=0; i < length; i++)
+	{
+		Serial.print((char) message[i]);
+		messageTemp += (char) message[i];
+	}
+
+	// Verifica que el mensaje se haya formado correctamente
+	Serial.println();
+	Serial.print("Mensaje guardado en una sola variable: ");
+	Serial.println(messageTemp);
+}
+
+// Función de reconexión
+void reconnect()
+{
+	// Bucle para realizar la conexión
+	while(!client.connected()) // Verifica si existe conexión
+	{
+		Serial.print("Intentando conexión...");
+		if(client.connect("codigoIoT/medidor/MQTT")) // Recibe resultado del intento de conexión
+		{
+			Serial.println("Conectado");
+			//client.subscribe("esp32/output"); // Suscripción al tema
+		}
+		else // Sin conexión
+		{
+			Serial.print("Fallo en conexión, Error rc=");
+			Serial.print(client.state()); // Imprime el código del error
+			Serial.println("Intento de conexión en 5 segundos");
+			// Espera de 5 segundos
+			delay(5000);
+			Serial.println(client.connected()); // Imprime estatus de conexión
+		}
+	}
 }
